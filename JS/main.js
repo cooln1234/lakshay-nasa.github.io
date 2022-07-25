2c3597dd474bc2cd43511847bdc252171c37f608
lakshaynasa <lakshay1717@gmail.com>
2022-07-26 02:12:17 +0530

    Feat:🚀 Updated

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+// Work Status Animation
+var typed = new Typed(".work-status-anim", {
+    strings: ["Full Stack Developer", "Open Source Advocate", "Animator", "Designer"],
+    typespeed: 100,
+    backspeed: 60,
+    loop: true,
+});
