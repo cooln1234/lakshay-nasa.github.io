caabc23c35bec3b12f461c955d18d12b72195db4
lakshaynasa <lakshay1717@gmail.com>
2022-09-06 23:08:25 +0530

    Tried Single Page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 // Work Status Animation
 var typed = new Typed(".work-status-anim", {
-    strings: ["Full Stack Developer", "Open Source Advocate", "Animator", "Designer"],
-    typespeed: 100,
-    backspeed: 60,
+    strings: ["Full Stack Developer", "Open Source Advocate", "UI/UX", "Designer"],
+    typespeed: 10,
+    backspeed: 10,
     loop: true,
 });
+
+
+// function openAbout()
+// {   
+//     console.log("About Triggered")
+// }
+
+
+function openAbout() {
+    var displayCheck = document.getElementById("overlay-about");
+    displayCheck.style.display = 'block';
+}
+
+function openProjects() {
+    var displayCheck = document.getElementById("overlay-about");
+    displayCheck.style.display = 'block';
+}
+
+function exit(){
+    var displayCheck = document.getElementById("overlay-about");
+    displayCheck.style.display = 'none';
+}
